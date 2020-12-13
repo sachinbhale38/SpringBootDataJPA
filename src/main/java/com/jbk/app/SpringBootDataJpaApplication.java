305cdc35bbe6204f045ad3f9a30b16694fdf5c08
Sachin Bhale <sachinbhale38@gmail.com>
2020-12-13 12:04:42 +0530

    converted to war

--- src/main/java/com/jbk/app/SpringBootDataJpaApplication.java
+++ src/main/java/com/jbk/app/SpringBootDataJpaApplication.java
@@ -3,13 +3,15 @@ package com.jbk.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.SpringServletContainerInitializer;
 
 @SpringBootApplication
 @ComponentScan("com.jbk")
 @EntityScan("com.jbk.model")
-public class SpringBootDataJpaApplication {
+public class SpringBootDataJpaApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataJpaApplication.class, args);
